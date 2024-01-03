38893070ed9257c6b5907de6cfbd1d7249f0bdbc
DjentMachine <44790709+DjentMachine@users.noreply.github.com>
2024-01-03 14:52:51 +0000

    Solution to Day 01

---
+++ Day 01/day01.py
@@ -0,0 +1,21 @@
+"""
+Advent of Code - Day 01
+
+@author: Diogo Barros
+"""
+
+import re
+
+values=[]
+
+with open("input.txt",'r') as text:
+    for line in text:
+        #Saving all int matches in a temp array
+        matches=re.findall(r'\d',line)
+        first = matches[0]
+        last = matches[-1]
+        #append the 2 digit number to the 'values' array
+        values.append(int(first+last))
+        
+print("Sum of all calibration values is: " + str(sum(values)))        
+     
